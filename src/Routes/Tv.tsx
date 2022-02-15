9929164fdbd2e9831d40790299f089ba0c7a1813
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2022-02-15 19:05:00 +0900

    final1

--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -19,7 +19,7 @@ const Gettv = styled(motion.div)`
   width: 100%;
   display: grid;
   gap: 15px;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   position: absolute;
   top: 150px;
 `;
@@ -27,7 +27,7 @@ const Gettoprate = styled(motion.div)`
   width: 100%;
   display: grid;
   gap: 15px;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   top: 450px;
   position: absolute;
 `;
@@ -36,7 +36,7 @@ const Getpopluate = styled(motion.div)`
   display: grid;
   gap: 15px;
   top: 750px;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   position: absolute;
 `;
 
@@ -45,6 +45,8 @@ const Tvitem = styled(motion.div)<{ bgphoto: string }>`
   height: 250px;
   background-image: url(${(props) => props.bgphoto});
   background-size: cover;
+  cursor: pointer;
+
   &:first-child {
     transform: left center;
   }
@@ -57,6 +59,7 @@ const Topitem = styled(motion.div)<{ bgphoto: string }>`
   height: 250px;
   background-image: url(${(props) => props.bgphoto});
   background-size: cover;
+  cursor: pointer;
   &:first-child {
     transform: left center;
   }
@@ -69,6 +72,7 @@ const Popitem = styled(motion.div)<{ bgphoto: string }>`
   height: 250px;
   background-image: url(${(props) => props.bgphoto});
   background-size: cover;
+  cursor: pointer;
   &:first-child {
     transform: left center;
   }
@@ -77,6 +81,82 @@ const Popitem = styled(motion.div)<{ bgphoto: string }>`
   }
 `;
 
+const Hovertitle = styled(motion.div)`
+  width: 100%;
+  position: absolute;
+  background: grey;
+  bottom: 0;
+  padding: 10px;
+  text-align: center;
+  opacity: 0;
+`;
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+
+const OverlayItem = styled.div`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: #2f2f2f;
+  display: flex;
+  flex-direction: column;
+`;
+
+const OverlayItemPic = styled.div<{ bgphoto: string }>`
+  width: 100%;
+  height: 60%;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+`;
+const OverlayItemInfo = styled.div`
+  width: 100%;
+  height: 40%;
+
+  padding: 10px 50px 10px 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const ItemInfoTitle = styled.h1`
+  margin-bottom: 10px;
+`;
+const ItemInfoDesc = styled.p`
+  margin-bottom: 30px;
+`;
+const ItemInfoPopularity = styled.h4`
+  margin-bottom: 10px;
+`;
+
+const CloseBtn = styled.button`
+  background: transparent;
+  border: none;
+  position: absolute;
+  right: 0;
+  padding: 10px;
+  font-size: 20px;
+`;
+
+const Home = styled(motion.img)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute;
+
+  right: 10px;
+`;
 function Tv() {
   const { isLoading: tvloading, data: tvdata } = useQuery<IgetTV>(
     ["tv", "get"],
@@ -97,13 +177,18 @@ function Tv() {
   const [tvLeaving, setTvLeaving] = useState(false);
   const [TopLeaving, setTopLeaving] = useState(false);
   const [PopLeaving, setPopLeaving] = useState(false);
-  const show = 6;
+  const show = 5;
+  const OnAirRouteMatch = useRouteMatch<{ id: string }>("/tv/onair/:id");
+  const TopRouteMatch = useRouteMatch<{ id: string }>("/tv/top/:id");
+  const PopRouteMatch = useRouteMatch<{ id: string }>("/tv/pop/:id");
+  const history = useHistory();
   const TvClick = () => {
     if (tvdata) {
       if (tvLeaving) return;
 
       const page = tvdata?.results.length;
-      const tvpage = Math.floor(page / show);
+      const tvpage = Math.floor(page / show) - 1;
+      console.log(tvpage);
       setTvLeaving(true);
       setTvindex((prev) => (prev === tvpage ? 0 : prev + 1));
     }
@@ -128,6 +213,13 @@ function Tv() {
     }
   };
 
+  const goHome = () => {
+    history.push("/");
+  };
+
+  const goTv = () => {
+    history.push("/tv");
+  };
   const Tvleaving = () => {
     setTvLeaving((prev) => !prev);
   };
@@ -137,6 +229,17 @@ function Tv() {
   const Popleaving = () => {
     setPopLeaving((prev) => !prev);
   };
+
+  const tvDetail = (id: string) => {
+    history.push(`/tv/onair/${id}`);
+  };
+  const topDetail = (id: string) => {
+    history.push(`/tv/top/${id}`);
+  };
+  const popDetail = (id: string) => {
+    history.push(`/tv/pop/${id}`);
+  };
+
   const rowVariants = {
     hidden: {
       x: window.outerWidth + 10,
@@ -148,7 +251,46 @@ function Tv() {
       x: -window.outerWidth - 10,
     },
   };
-  console.log("하위");
+
+  const ItemVariants = {
+    normal: {
+      scale: 1,
+    },
+    hover: {
+      scale: 1.3,
+      y: -80,
+      transition: {
+        delay: 0.5,
+        duration: 0.3,
+        type: "tween",
+      },
+    },
+  };
+  const HoverVariants = {
+    hover: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 0.3,
+        type: "tween",
+      },
+    },
+  };
+
+  const tvMatch = tvdata?.results.find(
+    (data) => data.id + "" === OnAirRouteMatch?.params.id
+  );
+
+  const topMatch = topratedata?.results.find(
+    (data) => data.id + "" === TopRouteMatch?.params.id
+  );
+
+  const popMatch = populatedata?.results.find(
+    (data) => data.id + "" === PopRouteMatch?.params.id
+  );
+
+  console.log(tvMatch);
+
   return (
     <Wrapper>
       {tvloading ? (
@@ -167,10 +309,14 @@ function Tv() {
               .slice(show * Tvindex, show * Tvindex + show)
               .map((item) => (
                 <Tvitem
+                  onClick={() => tvDetail(item.id + "")}
+                  variants={ItemVariants}
+                  initial="normal"
+                  whileHover="hover"
                   key={item.id}
                   bgphoto={makeImagePath(item.backdrop_path, "w500")}
                 >
-                  {item.name}
+                  <Hovertitle variants={HoverVariants}>{item.name}</Hovertitle>
                 </Tvitem>
               ))}
             <button
@@ -204,10 +350,14 @@ function Tv() {
               .slice(show * Topindex, show * Topindex + show)
               .map((item) => (
                 <Topitem
+                  onClick={() => topDetail(item.id + "")}
+                  variants={ItemVariants}
+                  initial="normal"
+                  whileHover="hover"
                   bgphoto={makeImagePath(item.backdrop_path, "w500")}
                   key={item.id}
                 >
-                  {item.name}
+                  <Hovertitle variants={HoverVariants}>{item.name}</Hovertitle>
                 </Topitem>
               ))}
             <button
@@ -241,10 +391,14 @@ function Tv() {
               .slice(show * Populateindex, show * Populateindex + show)
               .map((item) => (
                 <Popitem
+                  onClick={() => popDetail(item.id + "")}
+                  variants={ItemVariants}
+                  initial="normal"
+                  whileHover="hover"
                   bgphoto={makeImagePath(item.backdrop_path, "w500")}
                   key={item.id}
                 >
-                  {item.name}
+                  <Hovertitle variants={HoverVariants}>{item.name}</Hovertitle>
                 </Popitem>
               ))}
             <button
@@ -264,6 +418,81 @@ function Tv() {
           </Getpopluate>
         </AnimatePresence>
       )}
+      {OnAirRouteMatch ? (
+        <Overlay>
+          <OverlayItem style={{ bottom: "10%" }}>
+            <OverlayItemPic
+              bgphoto={makeImagePath(tvMatch?.backdrop_path, "w500")}
+            ></OverlayItemPic>
+            <OverlayItemInfo>
+              <ItemInfoTitle>TITLE : {tvMatch?.name}</ItemInfoTitle>
+              <ItemInfoDesc>
+                STORY:
+                <br />
+                {tvMatch?.overview}
+              </ItemInfoDesc>
+              <ItemInfoPopularity>
+                POPULARITY
+                <br />
+                <br />
+                {tvMatch?.popularity} SCORE
+              </ItemInfoPopularity>
+            </OverlayItemInfo>
+          </OverlayItem>
+        </Overlay>
+      ) : null}
+      {TopRouteMatch ? (
+        <Overlay>
+          <OverlayItem style={{ bottom: "10%" }}>
+            <OverlayItemPic
+              bgphoto={makeImagePath(topMatch?.backdrop_path, "w500")}
+            >
+              <CloseBtn onClick={goTv}>❌</CloseBtn>
+            </OverlayItemPic>
+            <OverlayItemInfo>
+              <Home
+                onClick={goHome}
+                src="https://cdn-icons-png.flaticon.com/512/609/609803.png"
+              ></Home>
+              <ItemInfoTitle>TITLE : {topMatch?.name}</ItemInfoTitle>
+              <ItemInfoDesc>
+                STORY:
+                <br />
+                {topMatch?.overview}
+              </ItemInfoDesc>
+              <ItemInfoPopularity>
+                POPULARITY
+                <br />
+                <br />
+                {topMatch?.popularity} SCORE
+              </ItemInfoPopularity>
+            </OverlayItemInfo>
+          </OverlayItem>
+        </Overlay>
+      ) : null}
+      {PopRouteMatch ? (
+        <Overlay>
+          <OverlayItem style={{ bottom: "10%" }}>
+            <OverlayItemPic
+              bgphoto={makeImagePath(popMatch?.backdrop_path, "w500")}
+            ></OverlayItemPic>
+            <OverlayItemInfo>
+              <ItemInfoTitle>TITLE : {popMatch?.name}</ItemInfoTitle>
+              <ItemInfoDesc>
+                STORY:
+                <br />
+                {popMatch?.overview}
+              </ItemInfoDesc>
+              <ItemInfoPopularity>
+                POPULARITY
+                <br />
+                <br />
+                {popMatch?.popularity} SCORE
+              </ItemInfoPopularity>
+            </OverlayItemInfo>
+          </OverlayItem>
+        </Overlay>
+      ) : null}
     </Wrapper>
   );
 }
