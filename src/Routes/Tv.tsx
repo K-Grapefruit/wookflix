952eb9f616bfb71bae33c243da7a313eece52498
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2022-02-18 16:01:23 +0900

    d

--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -122,7 +122,6 @@ const OverlayItemPic = styled.div<{ bgphoto: string }>`
 const OverlayItemInfo = styled.div`
   width: 100%;
   height: 40%;
-
   padding: 10px 50px 10px 50px;
   display: flex;
   flex-direction: column;
